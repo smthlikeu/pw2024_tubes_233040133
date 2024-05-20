d086177a702e0e67eca3a339425e8f0ce60fb2ba
Fauzan <azkaferdianto225@gmail.com>
2024-05-20 19:20:01 +0700

    membuat navbar

---
+++ layout/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <!-- Bootsrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="" alt="Ulinz" height="30">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#">Discover</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#">Trips</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#">Review</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#">More</a>
+          </li>
+        </ul>
+        <form class="d-flex align-items-center">
+          <a href="#" class="btn sign-in-btn ms-3">Sign in</a>
+          <a href="#" class="btn sign-up-btn ms-3">Sign up</a>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+
+
+
+
+ <!-- bootsrap -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
